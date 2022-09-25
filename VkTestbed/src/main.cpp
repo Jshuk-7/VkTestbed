3616db9347d6ea10f3706e929f459b79f9688ace
young_og <jessieshukeireh2001@gmail.com>
2022-09-25 11:58:26 -0400

    Setup Window and linked vk, glfw, and glm

---
+++ VkTestbed/src/main.cpp
@@ -0,0 +1,21 @@
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+int main(int argc, char** argv)
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Testbed", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+}
